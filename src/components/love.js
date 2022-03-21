90313bb36ae574a931c14183db66f8b2f3a73638
keshavdhande <keshavdhande1@gmail.com>
2022-03-21 18:57:16 +0530

    changed background color and solve result bug

--- src/components/love.js
+++ src/components/love.js
@@ -8,6 +8,7 @@ export default function Love(props) {
     const [randomm, setrandomm] = useState();
     const [loves, setloves] = useState('loves');
     const [per, setper] = useState('%');
+    const[result,setresult]=useState();
 
 
     const namechange = (event) => {
@@ -23,26 +24,33 @@ export default function Love(props) {
         const rand = Math.round(min + Math.random() * (max - min));
         
         setrandomm(Math.round(rand))
-        console.log(rand)
-      }
+        
+      var result =crush +" Loves "+ name +" "+ rand +" %";
+     
+        setresult(result)
+        
+    }
+
     return(
 <div>
  
-<img src={image} height={200} width={200} />
+<img src={image} height={300} width={300} />
 <h1 id="heading">love Calculator...</h1>
 
-<label htmlFor="">Your Name</label>
+<label htmlFor=""> <b>Your Name</b></label>
       
-      <input type="text" className="m-3"  placeholder="First name" value={name}  onChange={namechange}>
+      <input  type="text" className="m-3 box"  placeholder="First name" value={name}  onChange={namechange}>
       </input>
-      <label htmlFor="">Crush name: </label>
+      <label htmlFor=""> <b>Crush name:</b>Crush name: </label>
       
-      <input type="text" className="m-3" placeholder="first name" value={crush} onChange={crushchange}>
+      <input type="text" className="m-3 box" placeholder="first name" value={crush} onChange={crushchange}>
       </input>
       <br></br>
       <button id='btn' className="m-5"  onClick={handleClick}>Calculate</button>
 
-<h2 id='result'>{crush} loves {name} {randomm} % </h2>
+{/* <h2 id='result'>{crush} loves {name} {randomm} % </h2> */}
+ <h2 id='result'>{result}</h2> 
+
 </div>
     
     )
